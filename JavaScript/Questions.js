f0b0ca3ed522c589b9d190d065e9f016f1893686
Elias apenas <elias2004souzam@gmail.com>
2023-05-10 00:31:29 -0300

    Atualização 10/05/2023
    Adição do quiz e aperfeiçoamento do Bootstrap

---
+++ JavaScript/Questions.js
@@ -0,0 +1,34 @@
+export default [
+    {
+      question: "Qual o motor de um carro elétrico?",
+      answers: [
+        { option: "Motor a Indução", correct: true },
+        { option: "Motor Aspirado", correct: false },
+        { option: "Motor Rotatório", correct: false },
+      ],
+    },
+    {
+      question: "Quais benefícios ao Meio Ambiente?",
+      answers: [
+        { option: "Uso de Energia Renovável", correct: false },
+        { option: "Combustível mais barato", correct: false },
+        { option: "Todas as alternativas", correct: true },
+      ],
+    },
+    {
+      question: "Qual o consumo anual de um veículo elétrico?",
+      answers: [
+        { option: "R$ 3.267,00", correct: false },
+        { option: "R$ 533,00", correct: true },
+        { option: "R% 272,00", correct: false },
+      ],
+    },
+    {
+      question: "Qual o nome da Startup Brasileira de veículos elétricos?",
+      answers: [
+        { option: "BYD", correct: false },
+        { option: "Tracker", correct: false },
+        { option: "Mileto", correct: true },
+      ],
+    },
+  ];
